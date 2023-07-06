ba7ce7c34e3f809be4260157a0e0f4198016d45a
kyoungseok-Oh <134459438+kyoungseok-Oh@users.noreply.github.com>
2023-07-06 11:23:15 +0900

    Add files via upload

---
+++ ArrayMake.java
@@ -0,0 +1,15 @@
+public class ArrayMake {
+    public static void main(String[] args) {
+        //Q. 정수 5개를 저장 할 배열을 생성하는 방법
+        int[]a = new int[5];
+        //반복문 (for, while)
+        for (int i = 0; i < a.length; i++) {
+            a[i]=10;
+            System.out.println(a[i]);
+
+        }
+        float[] f=new float[5]; // 길이가 고정
+        Book b1,b2,b3;
+        Book[] b = new Book[3]; // 책3권을 저장할 배열을 생성
+    }
+}
